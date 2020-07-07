fd6720dc2a626a8bf7dd6ed63f6f3275e80505e3
sushilshipalkar <sushil.shipalkar2012@gmail.com>
2020-07-07 12:48:51 +0530

    updated value

--- WebApplication2/Controllers/ValuesController.cs
+++ WebApplication2/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace WebApplication2.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value5" };
         }
 
         // GET api/values/5
